6fccfd21ae2466a16aa7b06335db546b4c0fa4de
Taro <taro@example.com>
2024-01-11 18:14:21 +0900

    Keijiban

---
+++ src/main/java/com/example/board/entity/Post.java
@@ -0,0 +1,43 @@
+package com.example.board.entity;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * 投稿.
+ */
+@Entity
+@Table
+@Data
+public class Post {
+	/** ID */
+	@Id
+	@Column
+	private String id = null;
+	
+	/**投稿者*/
+	@Column(length = 20, nullable = false)
+	private String author = null;
+	
+	/**タイトル*/
+	@Column(length = 20, nullable = false)
+	private String title = null;
+	
+	/**内容*/
+	@Column
+	private String body = null;
+	
+	/**登録日時*/
+	private Date createdDate = null;
+	
+	/**更新日時*/
+	private Date updatedData = null;
+	
+	/** 削除済 */
+	private boolean deleted = false;
+	
+}
